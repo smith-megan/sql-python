e7e32d9380ba933e51b8145f8ea4c142d25ec65d
Megan <smith.megan.design@gmail.com>
2021-08-13 10:05:15 -0600

    finished python section commenting and edit

--- process.py
+++ process.py
@@ -1,12 +1,18 @@
 log_file = open("um-server-01.txt")
-
+# opens file for python to work with
 
 def sales_reports(log_file):
+# defines a function and feeds it the file
     for line in log_file:
+# Begins a loop of the file, setting line as our variable
         line = line.rstrip()
+# redefines the line variable to strip excess spaces
         day = line[0:3]
-        if day == "Tue":
+# sets the day variable to the first three letters of the line.
+        if day == "Mon":
+# if the day variable is equal to a string then...
             print(line)
-
+# print out that line to the console
 
 sales_reports(log_file)
+# calls the function and sets the argument as the opened file
